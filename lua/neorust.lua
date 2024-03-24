6496d41e01b40650287d07dfa694204119d7f5a7
LocalTrash <tqdagboy@gmail.com>
2024-03-24 10:33:28 -0700

    HELP ME

--- lua/neorust.lua
+++ lua/neorust.lua
@@ -3,7 +3,7 @@ local group = vim.api.nvim_create_augroup("ScratchBuffer", {  clear = true })
 local function create_buffer() 
 	local buffer = vim.api.nvim_create_buf(true, true)
 	vim.api.nvim_buf_set_name(buffer, "*scratch*")
-	vim.apo.nvim_set_option_value("filetype", "rs", {buffer = buffer})
+	vim.apo.nvim_set_option_value("filetype", "rs", {buf = buffer})
 	return buffer
 end
 
