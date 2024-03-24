a4f101facfeec35929d3f06a34e12cc8d948d85c
LocalTrash <tqdagboy@gmail.com>
2024-03-23 23:50:06 -0700

    Hope

--- lua/neorust.lua
+++ lua/neorust.lua
@@ -5,8 +5,7 @@ local function main()
 end
 
 local function setup()
-	vim.api.nvim_create_autocmd("VimEnter",
-	{ group = augroup, desc = "Set a fennel scratch buffer on load", once true, callback = main })
+	vim.api.nvim_create_autocmd("VimEnter", { group = augroup, desc = "Set a fennel scratch buffer on load", once true, callback = main })
 end
 
 return {setup = setup}
