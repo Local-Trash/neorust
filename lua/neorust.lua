03dce4389b4fd95105a838f9387e8af58deccd8b
LocalTrash <tqdagboy@gmail.com>
2024-03-23 23:39:57 -0700

    Test

--- lua/neorust.lua
+++ lua/neorust.lua
@@ -1,2 +1,12 @@
-print("help")
+local group = vim.api.nvim_create_augroup("ScratchBuffer", {  clear = true })
 
+local function main() {
+	print("Hellow")
+end
+
+local function setup() {
+	vim.api.nvim_create_autocmd("VimEnter",
+	{ group = augroup, desc = "Set a fennel scratch buffer on load", once true, callback = main })
+end
+
+return {setup = setup}
